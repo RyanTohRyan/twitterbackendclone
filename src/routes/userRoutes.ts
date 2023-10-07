b14433dde2593d89b031ea576d03d7484e73cd30
RyanTohRyan <70338573+RyanTohRyan@users.noreply.github.com>
2023-10-08 01:50:02 +0800

    couldnt curl, 1:!8

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,19 +1,24 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
-
+const prisma = new PrismaClient();
 
 router.post('/', (req,res)=>{
+    const {email, name, username} = req.body;
+    console.log(email, username,name);
     res.status(501).json({error: 'not implemented'});
 })
 
-router.get('/', (req,res)=>{
-    res.status(501).json({error: 'not implemented'});
+router.get('/', async (req,res)=>{
+    const allUser = await prisma.user.findMany()
+    res.json(allUser);
 })
 
-router.get('/:id', (req,res)=>{
+router.get('/:id', async (req,res)=>{
     const {id} = req.params;
-    res.status(501).json({error: `not implemented ${id}`});
+    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+    res.json(user);
 })
 
 router.put('/:id', (req,res)=>{
