4674efd7c16e162c848350ce6d10ce2f832f48b9
RyanTohRyan <70338573+RyanTohRyan@users.noreply.github.com>
2023-10-17 01:05:53 +0800

    2:20

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,19 +1,40 @@
+import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 
-router.post('/', (req,res)=>{
-    res.status(501).json({error: 'not implemented'});
+router.post('/', async (req,res)=>{
+    const {content, image, userId} = req.body;
+    
+    try{
+        const result = await prisma.tweet.create({
+            data: { content,image,userId} ,
+        })
+        res.status(200).json(result);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 })
 
-router.get('/', (req,res)=>{
-    res.status(501).json({error: 'not implemented'});
+router.get('/', async (req,res)=>{
+    const allTweets = await prisma.tweet.findMany({
+        include: {user:{select: {id: true, name: true,username: true,image: true}
+    }}});
+    res.status(200).json(allTweets);
 })
 
-router.get('/:id', (req,res)=>{
+router.get('/:id', async (req,res)=>{
     const {id} = req.params;
-    res.status(501).json({error: `not implemented ${id}`});
+    const tweet = await prisma.tweet.findUnique({where: {id: Number(id)},
+    include: {user: true}
+})
+    if(!tweet) {
+        console.log("kekw");
+       return res.status(404).json({error: "no tweet"});
+    }
+    res.status(200).json(tweet);
 })
 
 router.put('/:id', (req,res)=>{
@@ -21,8 +42,9 @@ router.put('/:id', (req,res)=>{
     res.status(501).json({error: 'not implemented'});
 })
 
-router.delete('/:id', (req,res)=>{
+router.delete('/:id', async (req,res)=>{
     const {id} = req.params;
+    await prisma.tweet.delete({where: {id: Number(id)}})
     res.status(501).json({error: 'not implemented'});
 })
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -21,13 +21,19 @@ router.post('/', async (req,res)=>{
 })
 
 router.get('/', async (req,res)=>{
-    const allUser = await prisma.user.findMany()
+    const allUser = await prisma.user.findMany({
+   //     select: {id: true, name: true, image: true}
+    })
     res.json(allUser);
 })
 
 router.get('/:id', async (req,res)=>{
     const {id} = req.params;
-    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+    const user = await prisma.user.findUnique({
+        where: {id: Number(id)},
+        include: {tweet: true}
+
+})
     res.json(user);
 })
 
